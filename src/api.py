f90e731204b9533fc484d373b012f5528252bf53
Laurence <30178403+LaurenceTaylor@users.noreply.github.com>
2020-04-13 13:43:33 +0100

    chore: make data json:api format (#6)

--- src/api.py
+++ src/api.py
@@ -2,19 +2,22 @@ import flask
 import db
 
 app = flask.Flask(__name__)
-app.config["DEBUG"] = True
+app.config['DEBUG'] = True
+
 
 def to_obj(entry):
-    return {'date': entry[1], 'temperature': entry[2], 'pressure': entry[3], 'humidity': entry[4], 'pm25': entry[5]}
+    return {'data': {'type': 'readings', 'id': entry[0], 'attributes': {'date': entry[1], 'temperature': entry[2], 'pressure': entry[3], 'humidity': entry[4], 'pm25': entry[5]}}}
 
-def to_json(data):
-    new_data = list(map(to_obj, data))
-    return flask.jsonify(new_data)
 
-@app.route('/', methods=['GET'])
+def readings_get():
+    data = db.get_readings()
+    transformed_data = list(map(to_obj, data))
+    return flask.jsonify(transformed_data)
+
+
+@app.route('/readings', methods=['GET'])
 def home():
-    readings = db.get_readings()
-    return to_json(readings)
+    return readings_get()
 
 
 app.run()
