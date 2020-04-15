b3eedc91540b724cea23da8b64c6e204fb7b80f9
Laurence <30178403+LaurenceTaylor@users.noreply.github.com>
2020-04-15 21:20:44 +0100

    chore: add querystring letting you set limit (#9)

--- src/api.py
+++ src/api.py
@@ -1,4 +1,5 @@
 import flask
+from flask import request
 import db
 import json
 
@@ -10,15 +11,17 @@ def to_obj(entry):
     return {'data': {'type': 'readings', 'id': entry[0], 'attributes': {'date': entry[1], 'temperature': entry[2], 'pressure': entry[3], 'humidity': entry[4], 'pm25': entry[5]}}}
 
 
-def readings_get():
-    data = db.get_readings()
+def readings_get(limit):
+    data = db.get_readings(limit)
     return list(map(to_obj, data))
 
 
 @app.route('/readings', methods=['GET'])
 def home():
-    try: 
-        return json.dumps(readings_get()), 200, {'ContentType': 'application/json'}
+    limit = request.args.get('limit')
+
+    try:
+        return json.dumps(readings_get(limit)), 200, {'ContentType': 'application/json'}
     except:
         return json.dumps({'status': 500, 'error': 'Internal Error'}), 500, {'ContentType': 'application/json'}
 
